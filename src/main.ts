a3c420a17a1af4239981e936d80d5fa735faa824
Cameron Coleman <142551249+TheSkyIsForward2@users.noreply.github.com>
2025-10-20 23:15:30 +0000

    Step 1 Complete - Title, Canvas, and Button Created

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
+document.title = "Sticker Sketchpad";
+
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+
+  <h1>Sticker Sketchpad</h1>
+  <canvas id="canvas" class="canvas" width = 256 height = 256></canvas> 
+  <button id="clearbutton" class="clearbutton"> Clear </button>
+
 `;
